cf5761b6b5facbfeee742cfb55c69a0b6b9a582b
DevosMathias <60935200+DevosMathias@users.noreply.github.com>
2020-11-25 18:29:45 +0100

    Besturingssystemen 2 docker.yml

--- src/main/java/domain/db/DbException.java
+++ src/main/java/domain/db/DbException.java
@@ -14,7 +14,7 @@ public class DbException extends RuntimeException {
 	public DbException(String message) {
 		super(message);
 	}
-	
+
 	
 	public DbException(Throwable exception) {
 		super(exception);
